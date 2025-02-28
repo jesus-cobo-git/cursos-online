7da1c7b2531d1cf88e33bae6c8340a6c93a623a4
Jesús Cobo <tu_correo@example.com>
2025-02-28 05:32:04 +0100

    Configuración para despliegue en Firebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind()
-  ],
-  output: 'server'
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
